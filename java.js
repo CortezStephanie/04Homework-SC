531ff521b9d44c8aed18d43635815efafd8f0d51
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-09-27 22:46:16 -0500

    pseudo code

---
+++ java.js
@@ -0,0 +1,8 @@
+// User will click a button to start quiz : use button 
+// timer starts : timer
+// timer presents a question 
+// ability to answer question
+// presented with another question after answering
+// question answered wrong take time off timer : if statement
+// game is over when all questions are answered or timer reaches 0
+// when game is over ability to save initials and save score
