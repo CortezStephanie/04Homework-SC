8c552d374b4b986c21bb20ee417c059f4b270efa
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-12-02 23:27:26 -0600

    functionality is ok

--- assets/script.js
+++ assets/script.js
@@ -14,39 +14,83 @@ var startBtnEl = document.querySelector(".start-btn");
 var submitBtnEl = document.querySelector(".submit-btn"); 
 var scoreEl = document.querySelector("#score-brd");
 var timerEl = document.querySelector(".timer");
+var questions = [{
+    questionTitle: "question1",
+    answerChoices: ["1", "2", "3", "12"],
+    answer: "12",
+},
+{
+    questionTitle: "question2",
+    answerChoices: ["4", "5", "6", "45"],
+    answer: "5",
+},
+{
+   questionTitle: "question3",
+    answerChoices: ["7", "8", "9", "78"],
+    answer: "8", 
+}];
 
 //timer
-var minsLeft = 2;
+var currentQuestion = 0;
+var secondsLeft = 30
 
 function setTime() {
     var timerInterval = setInterval(function(){
-        minsLeft--;
-    })
+        secondsLeft--;
+        timerEl.textContent = "Time:" + secondsLeft;
+
+        if(secondsLeft === 0 || currentQuestion >= questions.length) {
+            clearInterval(timerInterval);
+            endGame()
+        }
+    }, 1000);
 }
+
+function endGame () {
+    document.getElementById("quizSection").setAttribute("style", "display: none;")
+    document.getElementById("scoreInputSection").setAttribute("style", "display: block;")
+}
+
 // Start the game 
 
 startBtnEl.addEventListener("click", function(){
-    console.log("start button works")
-})
+    setTime();
+    document.getElementById("startSection").setAttribute("style", "display: none;")
+    document.getElementById("quizSection").setAttribute("style", "display: block;")
+    showQuestion()
+});
 
+function showQuestion() {
+    document.getElementById("questionText").textContent = questions[currentQuestion].questionTitle
+    document.getElementById("optionOne").textContent = questions[currentQuestion].answerChoices[0]
+    document.getElementById("optionTwo").textContent = questions[currentQuestion].answerChoices[1]
+    document.getElementById("optionThree").textContent = questions[currentQuestion].answerChoices[2]
+    document.getElementById("optionFour").textContent = questions[currentQuestion].answerChoices[3]
+    
+    
+    document.getElementById("optionOne").addEventListener("click", checkAnswer)
+    document.getElementById("optionTwo").addEventListener("click", checkAnswer)
+    document.getElementById("optionThree").addEventListener("click", checkAnswer)
+    document.getElementById("optionFour").addEventListener("click", checkAnswer)
+}
 
-
-//startBtnEl.addEventListener("click",);
+function checkAnswer(event) {
+    // console.log(event.target)
+    var pickedChoice = event.target.textContent;
+    // console.log(pickedChoice)
+    if(pickedChoice === questions[currentQuestion].answer) {
+        console.log("correct")
+    } else {
+        console.log("incorrect")
+        secondsLeft -= 5
+    }
+    // console.log(this)
+    currentQuestion++
+    if(currentQuestion < questions.length){
+        showQuestion()
+    }
+}
+//startBtnEl.addEventListener("click",); not correct
 // Questions
-/*
-var questions = [{
-    questionTitle: "question1",
-    answerChoices: ["1", "2", "3"],
-    answer: "2",
-},
-{
-    questionTitle: "question1",
-    answerChoices: ["1", "2", "3"],
-    answer: "2",
-},
-{
-   questionTitle: "question1",
-    answerChoices: ["1", "2", "3"],
-    answer: "2", 
-}];
-*/
+
+
